change normalization, fix tests for z_score (only).
More to come!

rendseq/zscores.py
@@ -21,13 +21,23 @@ def _add_padding(reads, gap, w_sz):
     return padded_reads
 
 
-def _get_means_sds(reads, w_sz):
+def _apply_std_dis_cutoff(window):
+    mask = np.abs(window - np.mean(window)) > 2 * np.std(window)
+    return window[~mask]
+
+
+def _get_means_sds(reads, w_sz, percent_trim=0, winsorize=False):
     """Calculate the arrays of mean and sd of tiled windows along data."""
-    sliding_windows = np.sort(
-        np.lib.stride_tricks.sliding_window_view(reads[:, 1], w_sz), axis=1
-    )[
-        :, : int(w_sz * 0.8)
-    ]  # Note - remove the to ~20% of values (other peaks?)
+    if percent_trim > 0:
+        sliding_windows = np.sort(
+            np.lib.stride_tricks.sliding_window_view(reads[:, 1], w_sz), axis=1
+        )[
+            :, : int(w_sz * (1 - percent_trim))
+        ]  # Note - remove the top ~x% of values (other peaks?)
+    else:
+        sliding_windows = np.lib.stride_tricks.sliding_window_view(reads[:, 1], w_sz)
+    if winsorize:
+        sliding_windows = np.apply_along_axis(_apply_std_dis_cutoff, 1, sliding_windows)
     means = np.mean(sliding_windows, axis=1)
     sds = np.std(sliding_windows, axis=1)
 
@@ -46,7 +56,7 @@ def _validate_gap_window(gap, w_sz):
         )
 
 
-def z_scores(reads, gap=5, w_sz=50):
+def z_scores(reads, gap=5, w_sz=50, percent_trim=0, winsorize=True):
     """Perform modified z-score transformation of reads.
 
     Parameters
@@ -56,6 +66,11 @@ def z_scores(reads, gap=5, w_sz=50):
             interest that should be excluded in the z_score calculation.
         -w_sz (integer): the max distance (in nt) away from the current position
             one should include in zscore calulcation.
+        -percent_trim - what fraction of the top reads should be dropped before
+            calculating the mean and std?  ie 0.1 means the top 10% of reads
+            are dropped.
+        -winsorize - bool for whether or not after trimming any reads more than
+            1.5 std from the mean should be dropped.
 
     Returns
     -------
@@ -66,8 +81,7 @@ def z_scores(reads, gap=5, w_sz=50):
     _validate_reads(reads)
     padded_reads = _add_padding(reads, gap, w_sz)
     pad_len = len(padded_reads[:, 0])
-    means, sds = _get_means_sds(padded_reads, w_sz)
-    print("calculated means and sds")
+    means, sds = _get_means_sds(padded_reads, w_sz, percent_trim, winsorize)
     upper_zscores = np.divide(
         np.subtract(
             padded_reads[gap + w_sz : pad_len - (gap + w_sz), 1],
@@ -75,7 +89,6 @@ def z_scores(reads, gap=5, w_sz=50):
         ),
         sds[(gap + w_sz) : len(means) - 1 - gap],
     )
-    print("calculated upper z scores")
     lower_zscores = np.divide(
         np.subtract(
             padded_reads[gap + w_sz : pad_len - (gap + w_sz), 1],
@@ -83,14 +96,13 @@ def z_scores(reads, gap=5, w_sz=50):
         ),
         sds[gap : len(means) - 1 - (gap + w_sz)],
     )
-    print("calculated lower z scores")
     zscores = padded_reads[gap + w_sz : pad_len - (gap + w_sz)].copy()
     zscores[:, 1] = np.min([lower_zscores, upper_zscores], axis=0)
 
     return zscores
 
 
-def parse_args_zscores(args):
+def _parse_args_zscores(args):
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
         description="Takes raw read file and\
@@ -137,7 +149,7 @@ def main_zscores():
     Effect: Writes messages to standard out. If --save-file flag,
     also writes output to disk.
     """
-    args = parse_args_zscores(sys.argv[1:])
+    args = _parse_args_zscores(sys.argv[1:])
 
     # Calculate z-scores
     filename = args.filename