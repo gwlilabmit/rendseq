cd9b88c4340ab3c333528ad0b329ec1c56034efb
Mirae Parker <miraep8@gmail.com>
2023-08-18 10:04:42 -0400

    changing the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@
 import os
 import sys
 
+import sphinx_readable_theme
+
 sys.path.insert(0, os.path.abspath("../.."))
 
 
@@ -55,7 +57,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "readable"
+html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
